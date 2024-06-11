5f7896f46accfe14335e3f3c9926a4d63d511aee
ffont28 <francesco.fontana4@studenti.unimi.it>
2024-06-11 08:58:23 +0000

    primo commit

---
+++ figurageometrica.java
@@ -0,0 +1,15 @@
+public interface figurageometrica {
+
+    /**
+     * Metodo che restituisce l'area della figura geometrica
+     * @return l'area di this
+     */
+    float area();
+
+    /**
+     * Metodo che restituisce il perimetro della figura geometrica
+     * @return il perimetro di this
+     */
+    float perimetro();
+
+}
---
+++ quadrilatero.java
@@ -0,0 +1,15 @@
+public class quadrilatero implements figurageometrica {
+
+    @Override
+    public float area() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'area'");
+    }
+
+    @Override
+    public float perimetro() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'perimetro'");
+    }
+    
+}
